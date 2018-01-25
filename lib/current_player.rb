b7b3680b55c7e58f0e4452bb50c85518aad32d2c
Jay Bednar <ironviralmedia@gmail.com>
2018-01-25 16:24:04 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board) 
   count = 0
   board.each do |position|
-    if board[index] != " " || board[index] != "" || board[index] != nil
+    if position != " " || position != "" || position != nil
       count += 1 
     end
   end 
