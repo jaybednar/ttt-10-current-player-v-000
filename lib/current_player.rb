8aca0f69447b2e9f0e2004121f83f6aee3d64fd2
Jay Bednar <ironviralmedia@gmail.com>
2018-01-25 16:36:48 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,10 +8,10 @@ def turn_count(board)
   return count
 end 
 
-def current_player(board)
-  if turn_count(board).even?
-    return "X"
-  else 
-    return "O"
-  end 
-end
+# def current_player(board)
+#   if turn_count(board).even?
+#     return "X"
+#   else 
+#     return "O"
+#   end 
+# end
